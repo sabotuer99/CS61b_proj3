6abec9f2f292ac4b1e56509f53f94d6c3c4c99a5
troy <sabotuer99@gmail.com>
2015-12-04 22:22:51 -0700

    Initial Commit

---
+++ Project3/src/trie/Trie.java
@@ -0,0 +1,17 @@
+package trie;
+
+/**
+ * Prefix-Trie. Supports linear time find() and insert(). 
+ * Should support determining whether a word is a full word in the 
+ * Trie or a prefix.
+ * @author 
+ */
+public class Trie {
+	
+    public boolean find(String s, boolean isFullWord) {
+		return false;
+    }
+
+    public void insert(String s) {
+    }
+}
